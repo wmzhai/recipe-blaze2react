b9cb8390b9f5cc20b7f30e7526ae24f06f8b2fde
Weiming Zhai <wmzhai@gmail.com>
2016-02-13 16:25:40 +0800

    menu

--- client/react/Menu.jsx
+++ client/react/Menu.jsx
@@ -1,7 +1,27 @@
 Menu = React.createClass({
   render() {
-    return (
-      <h1>React Menu</h1>
-    );
+
+    const allMenus = this.props.recipes.map((recipe) => {
+      return (
+        <div className="menu-item" key={recipe._id}>
+          <h3>{recipe.name}</h3>
+          <p>{recipe.desc}</p>
+        </div>
+      );
+    });
+
+
+    if (this.props.recipes.count() > 0) {
+      return (
+        <div>
+          {allMenus}
+        </div>
+      )
+    } else {
+      return (
+        <h3>请从 <a href="/recipes">菜谱</a> 里面选择菜品到菜单</h3>
+      )
+    }
+
   }
 });
